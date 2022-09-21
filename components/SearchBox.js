02a7019abd81bee2f935c8b92f0feb1e8af3085b
samiulislam09 <samiulislamsawon09@gmail.com>
2022-09-21 22:11:00 +0600

    added cities json file

---
+++ components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBox() {
+  return (
+      <div>
+          <input className='border border-black' type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default SearchBox
---
+++ pages/weather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBox from '../components/SearchBox'
+
+function weather() {
+  return (
+      <div>
+          <SearchBox></SearchBox>
+    </div>
+  )
+}
+
+export default weather
